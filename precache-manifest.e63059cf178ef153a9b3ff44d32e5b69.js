67413684560574994986a87d2d1ad523ec079654
Silas Groskreutz <50895337+silasgroskreutz@users.noreply.github.com>
2020-01-15 13:30:15 -0600

    Updates

---
+++ precache-manifest.e63059cf178ef153a9b3ff44d32e5b69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3758dfdc9b28ca174ab2a98777b23395",
+    "url": "/portfolio2/index.html"
+  },
+  {
+    "revision": "64e1e1609b772efe2179",
+    "url": "/portfolio2/static/css/main.9d858e39.chunk.css"
+  },
+  {
+    "revision": "f66624beedacded830a9",
+    "url": "/portfolio2/static/js/2.c108f941.chunk.js"
+  },
+  {
+    "revision": "bc0c83fb5c6c8b946b94c2ec61dd7b90",
+    "url": "/portfolio2/static/js/2.c108f941.chunk.js.LICENSE"
+  },
+  {
+    "revision": "64e1e1609b772efe2179",
+    "url": "/portfolio2/static/js/main.8af30f1c.chunk.js"
+  },
+  {
+    "revision": "31c54891172f02b65c03",
+    "url": "/portfolio2/static/js/runtime-main.31faba8b.js"
+  }
+]);
